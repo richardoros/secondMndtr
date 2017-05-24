d3cc0d29240d2c1415e0114a4568f05a44cbea6a
CasiKea <casianoros1223@gmail.com>
2017-05-24 19:11:02 +0200

    ImagePick
    ImagePicker using Image view and button to pick, the code is located into the controller folder.

---
+++ secondMndtr/mainWallControllerViewController.swift
@@ -0,0 +1,63 @@
+//
+//  mainWallControllerViewController.swift
+//  secondMndtr
+//
+//  Created by eak on 24/05/2017.
+//  Copyright © 2017 Richard and Casian. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class mainWallControllerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var theImage: UIImageView!
+    
+    
+    @IBAction func addImgBtn(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController (title: "Image source", message: "chose an image", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil )
+            }else{
+                print("Camera Dose not work")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated:true, completion:nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage ] as! UIImage
+        theImage.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+}
